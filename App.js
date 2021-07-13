54b4c50cf7f35df28dfeb1c091958f7c8f8c41d0
jayktailor1502 <jay.kumar@unthinkable.co>
2021-07-13 12:38:41 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import {View, Text} from 'react-native'
+
+function App() {
+  return (
+    <View>
+      <Text>Hi Hello....</Text>
+      <Text>Netflix</Text>
+    </View>
+  )
+}
+
+export default App
